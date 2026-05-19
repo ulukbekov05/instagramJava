089f5c265d32bf913510d7f0a0198f34ec73cd72
ulukbekov05 <samaganuluukvekov@gmail.com>
2026-05-19 16:25:14 +0600

    .

--- src/main/java/peaksoft/instagram/config/jwt/JwtFilter.java
+++ src/main/java/peaksoft/instagram/config/jwt/JwtFilter.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-    public class JwtFilter extends OncePerRequestFilter {
+        public class JwtFilter extends OncePerRequestFilter {
         private final JwtService jwtService;
 
         @Override
--- src/main/java/peaksoft/instagram/dto/SimpleResponse.java
+++ src/main/java/peaksoft/instagram/dto/SimpleResponse.java
@@ -12,4 +12,5 @@ import org.springframework.http.HttpStatus;
 public class SimpleResponse {
     HttpStatus httpStatus;
     String message;
+
 }
